0b3648109ce6170169bf6d12804b339f22acb649
Farid Faraji <farid.faraji@alumni.ubc.ca>
2018-12-25 19:13:45 -0500

    Updates

--- precache-manifest.33ad6dad492b77c26c6cfb713187e981.js
+++ precache-manifest.33ad6dad492b77c26c6cfb713187e981.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "1a306ac3aa675c9779ca",
-    "url": "/my-app/static/css/main.b84f34e9.chunk.css"
+    "revision": "a9b6c430668a9b895fec",
+    "url": "/my-app/static/css/main.4bc45f4a.chunk.css"
   },
   {
-    "revision": "1a306ac3aa675c9779ca",
-    "url": "/my-app/static/js/main.1a306ac3.chunk.js"
+    "revision": "a9b6c430668a9b895fec",
+    "url": "/my-app/static/js/main.a9b6c430.chunk.js"
   },
   {
     "revision": "a83f6ded40aa8ed32192",
@@ -32,12 +32,16 @@ self.__precacheManifest = [
     "url": "/my-app/static/media/logo.5d5d9eef.svg"
   },
   {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/my-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/my-app/static/media/outline-icons.ef60a4f6.woff"
   },
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/my-app/static/media/outline-icons.cd6c777f.woff2"
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/my-app/static/media/flags.9c74e172.png"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
@@ -46,10 +50,6 @@ self.__precacheManifest = [
   {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
     "url": "/my-app/static/media/outline-icons.ad97afd3.ttf"
-  },
-  {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/my-app/static/media/flags.9c74e172.png"
   },
   {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
@@ -76,12 +76,12 @@ self.__precacheManifest = [
     "url": "/my-app/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/my-app/static/media/brand-icons.c5ebe0b3.ttf"
-  },
-  {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/my-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/my-app/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
@@ -96,7 +96,7 @@ self.__precacheManifest = [
     "url": "/my-app/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "c70a2bd6667b38d74c5b8d1332f6a0c1",
+    "revision": "70eab58adbd21a083628e2307e4f3d9b",
     "url": "/my-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.bc0800238c7fccee67da246892ea2c25.js"
+  "/my-app/precache-manifest.33ad6dad492b77c26c6cfb713187e981.js"
 );
 
 workbox.clientsClaim();
