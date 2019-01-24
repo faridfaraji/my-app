0940adc9eeb8312fc86de6b37bba69c2b48e14c8
Farid Faraji <farid.faraji@alumni.ubc.ca>
2019-01-24 15:35:31 -0800

    Updates

---
+++ precache-manifest.3bd6b156aea70da5c569e3f42b78f162.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca533b882358328702f5a0df13ff7cda",
+    "url": "/my-app/static/media/facebook.ca533b88.ico"
+  },
+  {
+    "revision": "bdf58543a21a0ca3741f",
+    "url": "/my-app/static/css/main.aae099ac.chunk.css"
+  },
+  {
+    "revision": "6976c96a4793fa60b9d370410bef647e",
+    "url": "/my-app/static/media/in.6976c96a.ico"
+  },
+  {
+    "revision": "12d009550f46104c303d",
+    "url": "/my-app/static/js/1.12d00955.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "8e1e154297b4490c9f8d056a4c8751ea",
+    "url": "/my-app/static/media/mypic.8e1e1542.jpg"
+  },
+  {
+    "revision": "db2af8b8a3803025fd725e5155eeb274",
+    "url": "/my-app/static/media/graph.db2af8b8.png"
+  },
+  {
+    "revision": "69bc7ce069637e277853f92c2a68ab10",
+    "url": "/my-app/static/media/Bipartite.69bc7ce0.png"
+  },
+  {
+    "revision": "bdf58543a21a0ca3741f",
+    "url": "/my-app/static/js/main.bdf58543.chunk.js"
+  },
+  {
+    "revision": "ce856c9059217681f51088174a8aa155",
+    "url": "/my-app/static/media/RepProb.ce856c90.png"
+  },
+  {
+    "revision": "bfbf510a57ba87a658e5003afcffb18c",
+    "url": "/my-app/static/media/shikhar.bfbf510a.jpg"
+  },
+  {
+    "revision": "9aa291643b9608595e688e8f0c482cf5",
+    "url": "/my-app/static/media/Farid_Faraji_CV.9aa29164.pdf"
+  },
+  {
+    "revision": "ace0902b6604b821e2ff3b937601531f",
+    "url": "/my-app/static/media/wave.ace0902b.jpg"
+  },
+  {
+    "revision": "90855756747d4b6b4060a3b2fcdc1add",
+    "url": "/my-app/static/media/pytorch.90855756.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "12d009550f46104c303d",
+    "url": "/my-app/static/css/1.a56fd6a0.chunk.css"
+  },
+  {
+    "revision": "7dd847875bf90af267fe605dd18dff6d",
+    "url": "/my-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.471af95e91b3f3de8bd12b1b734865c1.js"
+  "/my-app/precache-manifest.3bd6b156aea70da5c569e3f42b78f162.js"
 );
 
 workbox.clientsClaim();
