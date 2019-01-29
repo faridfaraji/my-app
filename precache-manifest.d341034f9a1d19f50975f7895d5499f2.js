b06b26618e1c19e5cd5d7e004b389c2f8757dd81
Farid Faraji <farid.faraji@alumni.ubc.ca>
2019-01-29 14:30:23 -0800

    Updates

---
+++ precache-manifest.d341034f9a1d19f50975f7895d5499f2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca533b882358328702f5a0df13ff7cda",
+    "url": "/website/static/media/facebook.ca533b88.ico"
+  },
+  {
+    "revision": "677a926d4a5c25e91bfd",
+    "url": "/website/static/css/main.aae099ac.chunk.css"
+  },
+  {
+    "revision": "6976c96a4793fa60b9d370410bef647e",
+    "url": "/website/static/media/in.6976c96a.ico"
+  },
+  {
+    "revision": "12d009550f46104c303d",
+    "url": "/website/static/js/1.12d00955.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "8e1e154297b4490c9f8d056a4c8751ea",
+    "url": "/website/static/media/mypic.8e1e1542.jpg"
+  },
+  {
+    "revision": "db2af8b8a3803025fd725e5155eeb274",
+    "url": "/website/static/media/graph.db2af8b8.png"
+  },
+  {
+    "revision": "69bc7ce069637e277853f92c2a68ab10",
+    "url": "/website/static/media/Bipartite.69bc7ce0.png"
+  },
+  {
+    "revision": "677a926d4a5c25e91bfd",
+    "url": "/website/static/js/main.677a926d.chunk.js"
+  },
+  {
+    "revision": "ce856c9059217681f51088174a8aa155",
+    "url": "/website/static/media/RepProb.ce856c90.png"
+  },
+  {
+    "revision": "bfbf510a57ba87a658e5003afcffb18c",
+    "url": "/website/static/media/shikhar.bfbf510a.jpg"
+  },
+  {
+    "revision": "9aa291643b9608595e688e8f0c482cf5",
+    "url": "/website/static/media/Farid_Faraji_CV.9aa29164.pdf"
+  },
+  {
+    "revision": "ace0902b6604b821e2ff3b937601531f",
+    "url": "/website/static/media/wave.ace0902b.jpg"
+  },
+  {
+    "revision": "90855756747d4b6b4060a3b2fcdc1add",
+    "url": "/website/static/media/pytorch.90855756.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "12d009550f46104c303d",
+    "url": "/website/static/css/1.a56fd6a0.chunk.css"
+  },
+  {
+    "revision": "794b3414395f8f5a493734be6657a6ba",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba4632a42e4f6ccceaf28a82fb793f15.js"
+  "/website/precache-manifest.d341034f9a1d19f50975f7895d5499f2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
