c73087e7497f2823288ad3fc4235aeefbba7c876
Farid Faraji <farid.faraji@alumni.ubc.ca>
2019-02-03 11:40:16 -0800

    chamges

--- src/projects.js
+++ src/projects.js
@@ -42,9 +42,10 @@ return (
     icon ={<GoRepoForked />}
   > <div className ="subtimeline">
     <h3 className="vertical-timeline-element-title">CNN Image Classifier</h3>
-    <h4 className="vertical-timeline-element-subtitle"></h4>
+    <br/>
+    <h4 className="vertical-timeline-element-subtitle">An app that classifies images</h4>
     <p>
-      Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+      
     </p></div>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -53,10 +54,10 @@ return (
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     icon ={<FaFirstOrderAlt />}
   >
-    <h3 className="vertical-timeline-element-title">Kaggle Competition</h3>
-    <h4 className="vertical-timeline-element-subtitle"></h4>
+    <h3 className="vertical-timeline-element-title">Kaggle Competition</h3> <br/>
+    <h4 className="vertical-timeline-element-subtitle">Its a Kaggle competition</h4>
     <p>
-      Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+      
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -65,10 +66,10 @@ return (
     iconStyle={{ background: 'rgb(78, 77, 122)', color: '#fff' }}
     icon={<TiAdjustBrightness/>} 
   >
-    <h3 className="vertical-timeline-element-title">UBC MIST </h3>
-    <h4 className="vertical-timeline-element-subtitle">Vancouver, BC</h4>
+    <h3 className="vertical-timeline-element-title">UBC MIST </h3><br/>
+    <h4 className="vertical-timeline-element-subtitle">Health app that measures your stress</h4>
     <p>
-      User Experience, Visual Design
+      
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -77,10 +78,10 @@ return (
     iconStyle={{ background: 'rgb(202, 138, 234)', color: '#fff' }}
     icon={<IoIosGlobe/>} 
   >
-    <h3 className="vertical-timeline-element-title">Data Cleaning Software</h3>
-    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+    <h3 className="vertical-timeline-element-title">Data Cleaning Software</h3><br/>
+    <h4 className="vertical-timeline-element-subtitle">Cleans up the folder</h4>
     <p>
-      User Experience, Visual Design
+      
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -90,9 +91,9 @@ return (
     icon={<FaQq/>}
   >
     <h3 className="vertical-timeline-element-title">Style Transfer Software</h3>
-    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+    <h4 className="vertical-timeline-element-subtitle"></h4>
     <p>
-      Strategy, Social Media
+      
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -101,23 +102,15 @@ return (
     iconStyle={{ background: 'rgb(31, 149, 155)', color: '#fff' }}
     icon={<FaServer/>}
   >
-    <h3 className="vertical-timeline-element-title"></h3>
-    <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-    <p>
-      Creative Direction, User Experience, Visual Design
-    </p>
+ 
   </VerticalTimelineElement>
   <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="2002 - 2006"
+  
     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
     icon={<TiFlowSwitch/>}
   >
-    <h3 className="vertical-timeline-element-title"></h3>
-    <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-    <p>
-      Creative Direction, Visual Design
-    </p>
+  
+
   </VerticalTimelineElement>
   <VerticalTimelineElement
     iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
